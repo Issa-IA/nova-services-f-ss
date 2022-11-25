280410cbb82f7bb2f1487663f7ba1c9310b8a6c3
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2022-11-25 11:23:44 +0100

    Update inherit_list_sale.py

--- Inherit_list_sale/model/inherit_list_sale.py
+++ Inherit_list_sale/model/inherit_list_sale.py
@@ -5,15 +5,16 @@ class StockHerit(models.Model):
     _inherit = 'sale.order'
 
     etiquette_maintenance = fields.Selection([('maintenance', 'Maintenance'), ('autres', 'Autres'),('cvi','CVI')], compute='_maintenance_etiquette', store=True)
+    sale_new_contrat = fields.Integer(string="CONTRAT",default=0)
 
-
-    @api.depends('sale_maintnance','sale_dossier')
+    @api.depends('sale_maintnance','sale_dossier','sale_materiels_vendu')
     def _maintenance_etiquette(self):
         for rec in self:
             if rec.sale_maintnance :
                 rec.etiquette_maintenance = 'maintenance'
             elif rec.sale_dossier:
                 rec.etiquette_maintenance = 'cvi'
+                rec.sale_new_contrat = 1
             else:
                 rec.etiquette_maintenance = False
 
