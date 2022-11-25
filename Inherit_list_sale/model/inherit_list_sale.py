9854a4b2eca623ee933c4ad772440ee4b863dc68
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2022-11-25 10:58:09 +0100

    Update inherit_list_sale.py

--- Inherit_list_sale/model/inherit_list_sale.py
+++ Inherit_list_sale/model/inherit_list_sale.py
@@ -4,12 +4,16 @@ from odoo import models, fields, api
 class StockHerit(models.Model):
     _inherit = 'sale.order'
 
-    etiquette_maintenance = fields.Selection([('maintenance', 'Maintenance'), ('autres', 'Autres')], compute='_maintenance_etiquette', store=True)
+    etiquette_maintenance = fields.Selection([('maintenance', 'Maintenance'), ('autres', 'Autres'),('cvi','CVI')], compute='_maintenance_etiquette', store=True)
 
 
-    @api.depends('sale_maintnance')
+    @api.depends('sale_maintnance','sale_dossier','sale_materiels_vendu')
     def _maintenance_etiquette(self):
         for rec in self:
             if rec.sale_maintnance :
                 rec.etiquette_maintenance = 'maintenance'
+            elif rec.sale_dossier:
+                rec.etiquette_maintenance = 'cvi'
+            else:
+                rec.etiquette_maintenance = False
 
