8bfb449c51894ab351f4dcb753f2da4d33799c28
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2022-12-02 12:24:46 +0100

    Update delivery_inherit_model.py

--- delivery_inherit/model/delivery_inherit_model.py
+++ delivery_inherit/model/delivery_inherit_model.py
@@ -6,6 +6,17 @@ class StockHerit(models.Model):
 
     state = fields.Selection(selection_add=[('delivery', 'Bon de livraison'), ('done',)])
     delivery_sign = fields.Binary('Signature')
+    stock_block_1 = fields.Selection([('Normale', 'Normale'), ('Impaye', 'Impayé')],string="Statut de bon livraison",related="partner_id.x_studio_statut_de_compte")
+                                  
+    stock_block_yes_1 = fields.Selection([('Normale', 'Normale'), ('Impaye', 'Impayé')],string="Statut de bon livraison ok", compute="get_statu_compute_partner_1")
+   
+    @api.depends("stock_block_1")
+    def get_statu_compute_partner_1(self):
+        for rec in self:
+            if rec.picking_type_id.id == 2:
+                rec.stock_block_yes_1 = rec.stock_block_1
+            else:
+                rec.stock_block_yes_1 = 'Normale'
 
 
     def print_delivery(self):
