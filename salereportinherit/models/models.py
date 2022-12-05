6b69c68638c7e4b56af794c633b2f41302b4ca44
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2022-12-05 12:14:58 +0100

    Add files via upload

---
+++ salereportinherit/models/models.py
@@ -0,0 +1,13 @@
+from odoo import models, fields, api
+
+class SaleReportHerit(models.Model):
+    _inherit = 'sale.report'
+    sale_marge_report  = fields.Float(string="Marge commerciale", readonly=True)
+
+
+    def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
+        fields['sale_marge_report'] = ", SUM(s.sale_duree) AS sale_marge_report"
+        return super(SaleReportHerit, self)._query(with_clause, fields, groupby, from_clause)
+
+
+
